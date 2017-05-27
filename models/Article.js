10451937a77940a366381de16453074a8b7bc5e2
Jenny Chu <jennychu.sd@gmail.com>
2017-05-26 21:58:58 -0700

    added models

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+// Dependency
+var mongoose = require("mongoose");
+
+// Create the Schema class
+var Schema = mongoose.Schema;
+
+// Instantiate a ArticleSchema object with the Schema class we've made
+var ArticleSchema = new Schema({
+    headline: {
+        type: String,
+        required: "Title is required",
+        trim: true
+    },
+    link: {
+        type: String,
+        required: "Link is required",
+        trime: true,
+        unique: true
+    },
+    comments: [{
+        type: Schema.Types. ObjectId,
+        ref: "Comment"
+    }]
+});
+
+// Creat the Article model with our ArticleSchema schema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model to be used in server.js
+module.exports = Article;
