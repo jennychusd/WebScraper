0d2bd65e693c305260c5a173ff7b6068e689b410
Jenny Chu <jennychu.sd@gmail.com>
2017-05-26 23:37:22 -0700

    working on routes

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -7,13 +7,30 @@ var mongoose = require("mongoose");
 module.exports = function (app) {
     // Simple index route
     app.get("/", function(req, res) {
-        res.send(index.html);
+        res.render("index");
     });
     // Route to scrape data from another site
     app.get("/scrape", function (req, res) {
         request("http://www.cnn.com/health", function(error, response, html) {
+            if (error) {
+                console.log(error)
+            }
             var $ = cheerio.load(html);
+            $(".cd__headline").each(function(i, element) {
+                var headline = $(this).children("a").text();
+                var link = $(this).children("a").attr("href");
+            
+                if (headline && link) {
+                    var saveArticle = new Article({headline: headline, link: link});
+                    saveArticle.save(function(error, saved) {
+                        if (error) {
+                            console.log(error)
+                        }
+                    })
+                }
+            })
         })
+        res.redirect("/");
     });
     // Route to display saved articles
     app.get("/saved", function(req, res) {
